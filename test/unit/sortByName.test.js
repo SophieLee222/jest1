049a650adde3f5034cb474da276d46755dd8a5ee
sofia va <sophia.lily.lee@gmail.com>
2025-05-11 21:27:47 +0300

    Initial commit

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,21 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  test("Books names should be sorted in ascending order", () => {
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected); 
+  });
+});
